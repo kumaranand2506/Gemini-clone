a187dcd4338d4573feecf8d638f9cc61e700270e
Anand Kumar <kumaranand2506.ak@gmail.com>
2025-04-05 21:32:15 +0530

    new Updates

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,10 +9,35 @@ const Sidebar = () => {
             <img className='menu' src={assets.menu_icon} alt="" />
             <div className="new-chat">
                 <img src={assets.plus_icon} alt="" />
+                <p>New Chat</p>
+            </div>
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entery">
+                {/* <img src={assets.message_icon} alt="" /> */}
+                <img className='menu' src={assets.menu_icon} alt="" />
+                <p>Place to Visit in Bihar ..</p>
+                {/* <p>Tourist Places in Bihar ...</p> */}
+              </div>
             </div>
         </div>
         <div className="bottom">
-
+        <div className="bottom-item recent-entery">
+            <img src={assets.gem_spark} alt="" />
+            <p>Gem manager</p>
+          </div>
+          <div className="bottom-item recent-entery">
+            <img src={assets.question_icon} alt="" />
+            <p>Help</p>
+          </div>
+          <div className="bottom-item recent-entery">
+            <img src={assets.history_icon} alt="" />
+            <p>Activity</p>
+          </div>
+          <div className="bottom-item recent-entery">
+            <img src={assets.setting_icon} alt="" />
+            <p>Settings</p>
+          </div>
         </div>
     </div>
   )
